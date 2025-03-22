412b7292eb3bb8dbd00f9df6239e287ac04614ef
kripu77 <khadkakripu4@gmail.com>
2025-03-22 11:01:27 +1100

    read initial state from state machine

--- lib/store.ts
+++ lib/store.ts
@@ -1,19 +1,26 @@
+import { initalMindMapValue } from '@/app/data';
 import { create } from 'zustand';
+import { RefObject } from 'react';
 
 type MindmapState = {
   prompt: string;
   isLoading: boolean;
   mindmapData: string;
+  mindmapRef: RefObject<HTMLDivElement> | null;
   setPrompt: (prompt: string) => void;
   setIsLoading: (isLoading: boolean) => void;
   setMindmapData: (data: string) => void;
+  setMindmapRef: (ref: RefObject<HTMLDivElement>) => void;
 };
 
 export const useMindmapStore = create<MindmapState>((set) => ({
   prompt: '',
   isLoading: false,
-  mindmapData: '',
+  mindmapData: initalMindMapValue,
+  mindmapRef: null,
+  
   setPrompt: (prompt) => set({ prompt }),
   setIsLoading: (isLoading) => set({ isLoading }),
   setMindmapData: (data) => set({ mindmapData: data }),
+  setMindmapRef: (ref) => set({ mindmapRef: ref }),
 }));
