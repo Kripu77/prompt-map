132ff03a5997d49f3922b5014061168e78219ce3
kripu77 <khadkakripu4@gmail.com>
2025-03-22 11:02:53 +1100

    theme toggler

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -1,9 +1,6 @@
 "use client"
-
-import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
 
 export function ThemeToggle() {
@@ -11,10 +8,9 @@ export function ThemeToggle() {
 
   return (
     <Button
-      variant="ghost"
+      variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="fixed top-4 right-4"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -22,3 +18,4 @@ export function ThemeToggle() {
     </Button>
   )
 }
+
